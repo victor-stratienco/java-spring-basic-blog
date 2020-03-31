df6598566cfd640f8f99805bb832653738c1e572
victor-stratienco <victor.stratienco@gmail.com>
2020-03-31 16:05:37 -0400

    Completed module

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -4,12 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+
+import java.util.List;
+
 @Controller
 public class BlogController {
 
+    private PostRepository postRepository;
+
+    public BlogController(final PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
     @RequestMapping("/")
     public String listPosts(ModelMap model) {
-        model.put("title", "Blog Post 1");
+        List<Post> posts = postRepository.getAllPosts();
+        model.put("posts", posts);
         return "home";
     }
 }
